4d745c15f1f4f1ec757275212a21e501e59714c7
J <jmrumingan@gmail.com>
2019-10-14 23:05:24 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,7 +27,8 @@ def monopoly_with_third_tier
         four_pieces_owned: 200
       },
       names: {
-        1 => "", 2=> "", 3=> "", 4=> ""
+        1 => "", 
+        2=> "", 3=> "", 4=> ""
       }
     }
     
